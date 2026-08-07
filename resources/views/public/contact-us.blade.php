4873aa7b5e3ee1d400c317c037f7e9206efe4a06
unlistedgain247 <ankushdhawan012@gmail.com>
2026-08-07 15:21:53 +0530

    Changes

--- resources/views/public/contact-us.blade.php
+++ resources/views/public/contact-us.blade.php
@@ -93,7 +93,7 @@
 
             <div class="cn-social-grid">
                 <a href="https://x.com/Unlistedgain" target="_blank" class="cn-social-card cn-twitter">
-                    <img src="{{ asset('assets/img/twitter.png') }}" alt="Twitter">
+                    <svg viewBox="0 0 300 300.251" xmlns="http://www.w3.org/2000/svg" aria-label="X"><path fill="#fff" d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34 0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66"/></svg>
                     <span>Twitter / X</span>
                 </a>
                 <a href="https://www.facebook.com/profile.php?id=61587793225842" target="_blank" class="cn-social-card cn-facebook">
@@ -103,10 +103,6 @@
                 <a href="https://www.instagram.com/unlistedgain?igsh=MWU2ZXo1OGVuOHhvcQ==" target="_blank" class="cn-social-card cn-instagram">
                     <img src="{{ asset('assets/img/instagram-pink.png') }}" alt="Instagram">
                     <span>Instagram</span>
-                </a>
-                <a href="#" class="cn-social-card cn-telegram">
-                    <img src="{{ asset('assets/img/telegram.png') }}" alt="Telegram">
-                    <span>Telegram</span>
                 </a>
                 <a href="https://www.linkedin.com/company/unlistedgain-advantage-solutions-pvt-ltd/" target="_blank" class="cn-social-card cn-linkedin">
                     <img src="{{ asset('assets/img/linkedin-white.png') }}" alt="LinkedIn">
